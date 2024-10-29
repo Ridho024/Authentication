a1d51a916e266cf9af84fb26348a6ed17d3961ef
ridho777 <ridho.artsys@gmail.com>
2024-10-29 15:06:05 +0700

    Connect Session Sequelize

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,21 @@ import dotenv from "dotenv";
 import UserRoutes from "./routes/UserRoutes.js";
 import ProductRoutes from "./routes/ProductRoutes.js";
 import AuthRoutes from "./routes/AuthRoutes.js";
+import SequelizeStore from "connect-session-sequelize";
 import db from "./config/Database.js";
 dotenv.config();
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+const store = new sessionStore({ db: db });
 
 app.use(
   session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
+    store: store,
     cookie: {
       secure: "auto", // auto, jadi ketika menerima request http maka false, sebaliknya ketika menerima request https maka false
     },
@@ -33,5 +38,7 @@ app.use(express.json()); // Menerima data dengan format json
 app.use(UserRoutes);
 app.use(ProductRoutes);
 app.use(AuthRoutes);
+
+// store.sync();
 
 app.listen(process.env.APP_PORT, () => console.log(`Server listening on port: 5000`));

b85f0d148347111702c7ea77e88970e814387236
ridho777 <ridho.artsys@gmail.com>
2024-10-30 14:45:33 +0700

    Update product controller

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getProducts, getProductByID, createProduct, updateProduct, deleteProduct } from "../controllers/Products.js";
+import { verifyUser } from "../middleware/AuthUser.js"; // Biar nanti route hanya bisa diakses user yang telah login
 
 const router = express.Router();
-router.get("/products", getProducts);
-router.get("/product/data/:id", getProductByID);
-router.post("/products", createProduct);
-router.patch("/product/edit/:id", updateProduct);
-router.delete("/product/:id", deleteProduct);
+router.get("/products", verifyUser, getProducts);
+router.get("/product/data/:id", verifyUser, getProductByID);
+router.post("/products", verifyUser, createProduct);
+router.patch("/product/edit/:id", verifyUser, updateProduct);
+router.delete("/product/:id", verifyUser, deleteProduct);
 
 export default router;

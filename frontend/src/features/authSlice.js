9f79caba850fb0ba86e96a3ed1cea32e6ec0352f
ridho777 <ridho.artsys@gmail.com>
2024-11-02 14:07:37 +0700

    Update Get user login and logout

--- frontend/src/features/authSlice.js
+++ frontend/src/features/authSlice.js
@@ -24,6 +24,22 @@ export const LoginUser = createAsyncThunk("user/LoginUser", async (user, thunkAP
   }
 });
 
+export const getMe = createAsyncThunk("user/getMe", async (_, thunkAPI) => {
+  try {
+    const response = await axios.post("http://localhost:5000/me");
+    return response.data;
+  } catch (error) {
+    if (error.response) {
+      const message = error.response.data.msg;
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+});
+
+export const LogOut = createAsyncThunk("user/LogOut", async () => {
+    await axios.delete("http://localhost/logout")
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -31,6 +47,7 @@ export const authSlice = createSlice({
     reset: (state) => initialState,
   },
   extraReducers: (builder) => {
+    // LogIn
     builder.addCase(LoginUser.pending, (state) => {
       state.isLoading = true;
     });
@@ -46,6 +63,26 @@ export const authSlice = createSlice({
       state.isError = true;
       state.message = action.payload;
     });
+    
+    // GetMe => Get session information from api
+    builder.addCase(getMe.pending, (state) => {
+      state.isLoading = true;
+    });
+
+    builder.addCase(getMe.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+      state.user = action.payload;
+    });
+
+    builder.addCase(getMe.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.message = action.payload;
+    });
+
+
+
   },
 });
 
